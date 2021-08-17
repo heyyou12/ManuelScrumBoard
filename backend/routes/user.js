bc56fae81a9e58cf623438520a04d6059930e9d0
Manuel Restrepo <manolo.1217@hotmail.com>
2021-08-17 15:50:10 -0500

    implementation of auth and validateUSer in listUser

--- backend/routes/user.js
+++ backend/routes/user.js
@@ -1,9 +1,11 @@
 const express = require("express");
 const router = express.Router();
 const UserController = require("../controllers/user");
+const Auth = require("../middleware/auth");
+const ValidateUser = require("../middleware/validateUser");
 
 router.post("/registerUser", UserController.registerUser);
 //http://localhost:3002/api/user/listUser/
-router.get("/listUser/:name?", UserController.listUser);///:name? de esta forma el parametro es opcional, /:name sin el signo debe ir el parametro
+router.get("/listUser/:name?",Auth,ValidateUser,UserController.listUser);///:name? de esta forma el parametro es opcional, /:name sin el signo debe ir el parametro
 
 module.exports = router;
